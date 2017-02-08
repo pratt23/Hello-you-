4720031af5de0240e2780e9628ed0c4b3a6e84a4
pratt23 <pratt23@gmail.com>
2017-02-08 14:38:05 -0500

    Hello "You"
    This program prompt the user for input the name of the user  and print
    the name with a Hello as an output

---
+++ Prateek-Assignment-P1.py
@@ -0,0 +1,9 @@
+# Author: Prateek Tyagi
+# Description:  This program prompt the user for input the name of the user
+#               and print the name with a Hello as an output
+
+first_name = raw_input('Please tell me your first name: ') #Ask user to input first name
+
+last_name = raw_input('\nPlease tell me your last name: ') #Ask user to input last name
+
+print "\nHello",first_name, last_name # Print Hello and the user's input
